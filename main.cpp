60e7396c0dd60b3c86c20ac5f0fb7a20f8e84bd6
Épicier <louissarthou@gmail.com>
2026-08-12 01:06:41 +0200

    add counter clock wise moves

--- main.cpp
+++ main.cpp
@@ -4,27 +4,63 @@
 
 
 int main(int argc, const char* argv[]) {
+    
     Permutation<NC> q;
-    
-    
-    for (auto m : {U, R, F, L, B, bR, D, bL}){
-        q.compose(CP[m]);
-        q.compose(CP[m]);
-        q.compose(CP[m]);
+    for (auto m : {U, U2, R, R2, F, F2, L, L2, B, B2, bR, bR2, D, D2, bL, bL2}){
+        corner_apply(q, {m, m, m});
         assert(q.is_solved());
     }
+
+    corner_apply(q, {R, U, R, U, R, U, R, U, R, U, R, U});
+    assert(q.is_solved());
+    corner_apply(q, {R, F, R, F, R, F, R, F, R, F, R, F});
+    assert(q.is_solved());
+    corner_apply(q, {R, bR, R, bR, R, bR, R, bR, R, bR, R, bR});
+    assert(q.is_solved());
+    corner_apply(q, {B, bR, B, bR, B, bR, B, bR, B, bR, B, bR});
+    assert(q.is_solved());
+    corner_apply(q, {D, bR, D, bR, D, bR, D, bR, D, bR, D, bR});
+    assert(q.is_solved());
+    corner_apply(q, {D, F, D, F, D, F, D, F, D, F, D, F});
+    assert(q.is_solved());
+    corner_apply(q, {D, bR, D, bR, D, bR, D, bR, D, bR, D, bR});
+    assert(q.is_solved());
+    corner_apply(q, {U, L, U, L, U, L, U, L, U, L, U, L});
+    assert(q.is_solved());
+    corner_apply(q, {U, B, U, B, U, B, U, B, U, B, U, B});
+    assert(q.is_solved());
+    corner_apply(q, {bL, B, bL, B, bL, B, bL, B, bL, B, bL, B});
+    assert(q.is_solved());
+    corner_apply(q, {bL, L, bL, L, bL, L, bL, L, bL, L, bL, L});
+    assert(q.is_solved());
     
     Permutation<NE> r;
-    for (auto m : {U, R, F, L, B, bR, D, bL}){
-        r.compose(EP[m]);
-        r.compose(EP[m]);
-        r.compose(EP[m]);
+    for (auto m : {U, U2, R, R2, F, F2, L, L2, B, B2, bR, bR2, D, D2, bL, bL2}){
+        edge_apply(r, {m, m, m});
         assert(r.is_solved());
     }
 
-    for (auto m : {R, U, R, U, R, U, R, U, R, U}){
-        r.compose(EP[m]);
-    }
+    edge_apply(r, {R, U, R, U, R, U, R, U, R, U});
+    assert(r.is_solved());
+    edge_apply(r, {R, F, R, F, R, F, R, F, R, F});
+    assert(r.is_solved());
+    edge_apply(r, {R, bR, R, bR, R, bR, R, bR, R, bR});
+    assert(r.is_solved());
+    edge_apply(r, {B, bR, B, bR, B, bR, B, bR, B, bR});
+    assert(r.is_solved());
+    edge_apply(r, {D, bR, D, bR, D, bR, D, bR, D, bR});
+    assert(r.is_solved());
+    edge_apply(r, {D, F, D, F, D, F, D, F, D, F});
+    assert(r.is_solved());
+    edge_apply(r, {D, bR, D, bR, D, bR, D, bR, D, bR});
+    assert(r.is_solved());
+    edge_apply(r, {U, L, U, L, U, L, U, L, U, L});
+    assert(r.is_solved());
+    edge_apply(r, {U, B, U, B, U, B, U, B, U, B});
+    assert(r.is_solved());
+    edge_apply(r, {bL, B, bL, B, bL, B, bL, B, bL, B});
+    assert(r.is_solved());
+    edge_apply(r, {bL, L, bL, L, bL, L, bL, L, bL, L});
     assert(r.is_solved());
     return 0;
 }
--- move.hpp
+++ move.hpp
@@ -1,12 +1,13 @@
 #pragma once
 #include "utils.hpp"
 
-constexpr unsigned NMOVES = 8;
+constexpr unsigned NMOVES = 16;
 constexpr unsigned NC = 6;
 constexpr unsigned NE = 12;
 constexpr unsigned NT = 12;
 
-enum Moves : unsigned {U, R, F, L, B, bR, D, bL};
+enum Move : unsigned {U, U2, R, R2, F, F2, L, L2, B, B2, bR, bR2, D, D2, bL, bL2};
+using Sequence = std::vector<Move>;
 
 template<unsigned N>
 struct Permutation : std::array<unsigned, N> {
@@ -49,23 +50,51 @@ std::array<T, N> permute(const std::array<T, N> &items, const Permutation<NC> &p
 // Corner permutations
 static Permutation<NC> CP[NMOVES] {
     {3,1,2,4,0,5}, // U
+    {4,1,2,0,3,5}, // U'
     {2,1,3,0,4,5}, // R
+    {3,1,0,2,4,5}, // R'
     {1,2,0,3,4,5}, // F
+    {2,0,1,3,4,5}, // F'
     {4,0,2,3,1,5}, // L
+    {1,4,2,3,0,5}, // L'
     {0,1,2,5,3,4}, // B
+    {0,1,2,4,5,3}, // B'
     {0,1,5,2,4,3}, // bR
+    {0,1,3,5,4,2}, // bR'
     {0,5,1,3,4,2}, // D
-    {0,4,2,3,5,1}  // bL
+    {0,2,5,3,4,1}, // D'
+    {0,4,2,3,5,1}, // bL
+    {0,5,2,3,1,4}, // bL'
 };
+
+void corner_apply(Permutation<NC> &p, const Sequence &seq) {
+    for (auto m : seq){
+        p.compose(CP[m]);
+    }
+}
 
 // Edge permutations
 static Permutation<NE> EP[NMOVES] {
     {1,6,2,3,4,5,0,7,8,9,10,11}, // U
+    {6,0,2,3,4,5,1,7,8,9,10,11}, // U'
     {0,2,5,3,4,1,6,7,8,9,10,11}, // R
+    {0,5,1,3,4,2,6,7,8,9,10,11}, // R'
     {0,1,3,4,2,5,6,7,8,9,10,11}, // F
+    {0,1,4,2,3,5,6,7,8,9,10,11}, // F'
     {7,1,2,0,4,5,6,3,8,9,10,11}, // L
+    {3,1,2,7,4,5,6,0,8,9,10,11}, // L'
     {0,1,2,3,4,5,10,7,8,9,11,6}, // B
+    {0,1,2,3,4,5,11,7,8,9,6,10}, // B'
     {0,1,2,3,4,9,6,7,8,10,5,11}, // bR
+    {0,1,2,3,4,10,6,7,8,5,9,11}, // bR
     {0,1,2,3,8,5,6,7,9,4,10,11}, // D
+    {0,1,2,3,9,5,6,7,4,8,10,11}, // D'
     {0,1,2,3,4,5,6,11,7,9,10,8},  // bL
+    {0,1,2,3,4,5,6,8,11,9,10,7}, // bL'
 };
+
+void edge_apply(Permutation<NE> &p, const Sequence &seq) {
+    for (auto m : seq){
+        p.compose(EP[m]);
+    }
+}
