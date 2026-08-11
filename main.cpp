a000f6be173c565077554548e540ce032682b26f
Épicier <louissarthou@gmail.com>
2026-08-11 21:35:06 +0200

    edge permutations

--- main.cpp
+++ main.cpp
@@ -6,13 +6,25 @@
 int main(int argc, const char* argv[]) {
     Permutation<NC> q;
     
-
+    
     for (auto m : {U, R, F, L, B, bR, D, bL}){
         q.compose(CP[m]);
         q.compose(CP[m]);
         q.compose(CP[m]);
         assert(q.is_solved());
     }
+    
+    Permutation<NE> r;
+    for (auto m : {U, R, F, L, B, bR, D, bL}){
+        r.compose(EP[m]);
+        r.compose(EP[m]);
+        r.compose(EP[m]);
+        assert(r.is_solved());
+    }
 
+    for (auto m : {R, U, R, U, R, U, R, U, R, U}){
+        r.compose(EP[m]);
+    }
+    assert(r.is_solved());
     return 0;
 }
--- move.hpp
+++ move.hpp
@@ -37,6 +37,15 @@ struct Permutation : std::array<unsigned, N> {
     }
 };
 
+template<typename T, unsigned N>
+std::array<T, N> permute(const std::array<T, N> &items, const Permutation<NC> &perm) {
+    std::array<T, N> ret = items;
+    for (unsigned k = 0; k < N; ++k){
+        ret(k) = items[perm[k]];
+    }
+    return ret;
+}
+
 // Corner permutations
 static Permutation<NC> CP[NMOVES] {
     {3,1,2,4,0,5}, // U
@@ -47,4 +56,16 @@ static Permutation<NC> CP[NMOVES] {
     {0,1,5,2,4,3}, // bR
     {0,5,1,3,4,2}, // D
     {0,4,2,3,5,1}  // bL
+};
+
+// Edge permutations
+static Permutation<NE> EP[NMOVES] {
+    {1,6,2,3,4,5,0,7,8,9,10,11}, // U
+    {0,2,5,3,4,1,6,7,8,9,10,11}, // R
+    {0,1,3,4,2,5,6,7,8,9,10,11}, // F
+    {7,1,2,0,4,5,6,3,8,9,10,11}, // L
+    {0,1,2,3,4,5,10,7,8,9,11,6}, // B
+    {0,1,2,3,4,9,6,7,8,10,5,11}, // bR
+    {0,1,2,3,8,5,6,7,9,4,10,11}, // D
+    {0,1,2,3,4,5,6,11,7,9,10,8},  // bL
 };
