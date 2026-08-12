7e8f0b05081049a728ed6de5aeb9affe90277751
Épicier <louissarthou@gmail.com>
2026-08-12 22:36:52 +0200

    small change

--- fto.hpp
+++ fto.hpp
@@ -8,18 +8,21 @@ constexpr unsigned NT = 12;
 
 enum Move : unsigned {U, U2, R, R2, F, F2, L, L2, B, B2, bR, bR2, D, D2, bL, bL2};
 
-using Sequence = std::vector<Move>;
-
-void print(const Sequence &seq, const bool show_length=true, const bool line_break=true){
-    static const std::string notation[NMOVES]
-        {"U", "U'", "R", "R'", "F", "F'", "L", "L'", "B", "B'", "bR", "bR'", "D", "D'", "bL", "bL'"};
-
-    for (const auto m : seq) {
-        std::cout << notation[m] << " ";
+struct Sequence : std::vector<Move> {
+    template<typename... Args>
+    Sequence(Args... args) : std::vector<Move>{{ args... }} {} // Constructeur par brace-enclosed
+    void show(const bool show_length=true, const bool line_break=true) const {
+        static const std::string notation[NMOVES]
+            {"U", "U'", "R", "R'", "F", "F'", "L", "L'", "B", "B'", "bR", "bR'", "D", "D'", "bL", "bL'"};
+        
+        for (const auto m : *this) {
+            std::cout << notation[m] << " ";
+        }
+        
+        if (show_length) std::cout << "(" << size() << ")";
+        if (line_break) std::cout << std::endl;
     }
-    if (show_length) std::cout << "(" << seq.size() << ")";
-    if (line_break) std::cout << std::endl;
-}
+};
 
 template<unsigned N>
 struct Permutation : std::array<unsigned, N> {
--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "utils.hpp"
-#include "move.hpp"
+#include "fto.hpp"
 #include<cassert>
 
 static Sequence sequences[5] {{R, U}, {R, U2}, {R, U, L, F}, {R2, L, R, L2}, {R, B2, R2, D, L2}};
 
 int main(int argc, const char* argv[]) {
     for (const auto seq : sequences) {
-        print(seq, false, false); 
+        seq.show(false, false); 
         print(":", order(seq));
     }
     return 0;
