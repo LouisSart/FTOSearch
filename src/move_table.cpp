bdf6999efff905a2aff12ad771fb53463833f9e3
Épicier <louissarthou@gmail.com>
2026-08-28 16:14:18 +0200

    move tables for the new edges coordinates

--- src/move_table.cpp
+++ src/move_table.cpp
@@ -2,8 +2,10 @@
 #include "permutation.hpp"
 #include "fto.hpp"
 
-static constexpr unsigned SIX_EDGE_CARD = Layout<NE, 6>::CARD * Permutation<6>::CARD;
-static constexpr unsigned SIX_EDGE_EVEN_CARD = Layout<NE, 6>::CARD * Permutation<6, true>::CARD;
+static constexpr unsigned SIX_EDGE_PERM_CARD = Permutation<6>::CARD;
+static constexpr unsigned SIX_EDGE_EVEN_PERM_CARD = Permutation<6>::CARD / 2;
+static constexpr unsigned SIX_EDGE_CARD = Layout<NE, 6>::CARD * SIX_EDGE_PERM_CARD;
+static constexpr unsigned SIX_EDGE_EVEN_CARD = Layout<NE, 6>::CARD * SIX_EDGE_EVEN_PERM_CARD;
 static MoveTable<CORNER_CARD, NMOVES> cmt;
 static MoveTable<SIX_EDGE_CARD, NMOVES> emt1;
 static MoveTable<SIX_EDGE_EVEN_CARD, NMOVES> emt2;
@@ -27,23 +29,24 @@ bool load_move_tables() {
 // Split edges into two parts otherwise the move table is 15 GB lool
 // First part is a 6 edge partial permutation (any parity)
 unsigned e1_index(const CubieFTO& fto){
-    return fto.ep.partial_index<6>({0, 1, 2, 3, 4, 5});
+    auto [cl, c1, c2] = fto.ep.split_indices();
+    return cl * SIX_EDGE_PERM_CARD + c1;
 }
 
 void e1_from_index(const unsigned &c, CubieFTO &fto) {
-    fto.ep.set_from_partial_index<6>(c, {0, 1, 2, 3, 4, 5});
+    fto.ep.set_from_split_indices(c / SIX_EDGE_PERM_CARD, c % SIX_EDGE_PERM_CARD, 0);
 }
 
 // Second part takes care of the last 6 edges partial
 // perm but ignores the actual permutation of the last 2
 // because it is forced by the parity of the first set e1
 unsigned e2_index(const CubieFTO& fto){
-    // CHECKME : should be partial_index<6, true>
-    return fto.ep.partial_index<6, true>({6, 7, 8, 9, 10, 11});
+    auto [cl, c1, c2] = fto.ep.split_indices();
+    return cl * SIX_EDGE_EVEN_PERM_CARD + c2;
 }
 
 void e2_from_index(const unsigned &c, CubieFTO &fto) {
-    fto.ep.set_from_partial_index<6, true>(c, {6, 7, 8, 9, 10, 11});
+    fto.ep.set_from_split_indices(c / SIX_EDGE_EVEN_PERM_CARD, 0, c % SIX_EDGE_EVEN_PERM_CARD);
 }
 
 void generate_move_tables() {
@@ -53,10 +56,10 @@ void generate_move_tables() {
     tmt.compute<CubieFTO>(tri1_index, tri1_from_index, moves);
     tmt.write(triangle_mtable_path);
 
-    emt1.compute<CubieFTO, true>(e1_index, e1_from_index, moves);
+    emt1.compute<CubieFTO>(e1_index, e1_from_index, moves);
     emt1.write(edge_mtable_path_1);
 
-    emt2.compute<CubieFTO, true>(e2_index, e2_from_index, moves);
+    emt2.compute<CubieFTO>(e2_index, e2_from_index, moves);
     emt2.write(edge_mtable_path_2);
 }
 
