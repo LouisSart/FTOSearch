538bfea1147d01e6dac1f80b527e4cd0c7b6c595
Épicier <louissarthou@gmail.com>
2026-09-10 23:51:02 +0200

    main does a random move solve at depth 13 and times it

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,21 @@
 
 int main(int argc, const char* argv[]) {
     load_move_tables();
+    // generate_pruning_tables();
     load_pruning_tables();
 
-    auto scramble = random_moves(18);
-    scramble.show();
-    CubieFTO cfto;
-    cfto.apply(scramble);
-    FTO fto(cfto);
 
-    auto solutions = optimal(fto);
-    solutions.show<Move>();
+    auto scramble = random_moves(13);
+    scramble.show();
+
+    FTO fto;
+    fto.apply(scramble);
+
+    auto f = [&fto](){
+        auto solutions = optimal(fto);
+        solutions.show<Move>();
+    };
+    time_fn(f);
 
     return 0;
 }
